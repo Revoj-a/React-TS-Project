a28f46d6a1363c2d1a1d0addf015a1b912134cd2
Revoj-a <revojjoever@gmail.com>
2025-12-17 21:28:01 +0800

    Add Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import Button from "./components/Button";
+import Container from "./components/Container";
 import Input from "./components/Input";
+
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" />
-      <Input id="age" label="Your age" />
+      <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button>A Button</Button>
+      </p>
+      <p>
+        <Button href="https://google.com">A Link</Button>
+      </p>
+      <Container as={Button}>Click Me</Container>
     </main>
   );
 }
