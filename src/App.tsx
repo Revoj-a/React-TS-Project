8d14ee30e3f447ae326f85e3be340f35fc0935c2
Revoj-a <revojjoever@gmail.com>
2025-12-17 22:30:01 +0800

    Exposing Component APIs with useImperative

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
-import Form from "./components/Form";
+import Form, { type FormHandle } from "./components/Form";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string; test: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input id="name" label="Your name" type="text" />
         <Input id="age" label="Your age" type="number" />
         <Input label="Test" id="test" ref={input} type="text" />
