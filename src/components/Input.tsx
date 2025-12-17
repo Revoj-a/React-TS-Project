59ddff6cdc27bf3c0b70166724799536a51f13b1
Revoj-a <revojjoever@gmail.com>
2025-12-17 19:26:34 +0800

    Create form with input box

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,15 @@
-const Input = () => {
-  return <p></p>;
+interface InputProps {
+  label: string;
+  id: string;
+}
+
+const Input = ({ label, id }: InputProps) => {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
 };
 
 export default Input;
