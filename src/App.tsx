2014df9a0b3308f726568edb74bd11d393d1051b
Revoj-a <revojjoever@gmail.com>
2025-12-17 22:07:41 +0800

    Build wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string; test: string };
+    console.log(extractedData);
+  }
+
   const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
-      <Input label="Test" id="test" ref={input} />
-      <p>
-        <Button>A Button</Button>
-      </p>
+      <Form onSave={handleSave}>
+        <Input id="name" label="Your name" type="text" />
+        <Input id="age" label="Your age" type="number" />
+        <Input label="Test" id="test" ref={input} type="text" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
       <p>
         <Button href="https://google.com">A Link</Button>
       </p>
