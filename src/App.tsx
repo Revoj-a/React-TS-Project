dcc56bb1e71d1d060024bf2edea84c0fd4879f47
Revoj-a <revojjoever@gmail.com>
2025-12-17 21:43:17 +0800

    Add props in Input Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
+      <Input label="Test" id="test" ref={input} />
       <p>
         <Button>A Button</Button>
       </p>
